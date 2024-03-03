59deb07ffb5cc431aaeb140ba9d9675e9b5ef6f7
EhsanMasoumipor <emasoumipor@gmail.com>
2024-03-03 22:18:50 +0330

    Logged out implemented successfullygit add .

---
+++ src/app/api/auth/logout/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({
+            success: true,
+            message: "You Logged out successfully!!"
+        }, { status: 200 })
+
+        response.cookies.set({
+            name: "access_token",
+            value: '',
+            httpOnly: true,
+            expires: new Date(Date.now()),
+        });
+        return response;
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: error.message
+        }, { status: 500 })
+    }
+}
