85f3d2a0c7d9b43b42b6c183fa7c18a2c4863e51
EhsanMasoumipor <emasoumipor@gmail.com>
2024-03-11 23:43:19 +0330

    Show My orders in dashboard, && single page

---
+++ src/app/api/dashboard/myOrders/[id]/route.js
@@ -0,0 +1,22 @@
+import { connect } from "@/config/DB";
+import Order from "@/models/Order";
+import { NextResponse } from "next/server";
+
+connect()
+
+export async function GET(request, { params }) {
+    try {
+        const { id } = params;
+        const order = await Order.findOne({ _id: id })
+        if (order) {
+            return NextResponse.json({ order }, { status: 200 })
+        } else {
+            return NextResponse.json({ message: "Order not found" }, { status: 500 });
+        }
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: error.message
+        }, { status: 500 })
+    }
+}
