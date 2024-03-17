72dd9f11e046b99b81708135542a868117921b0b
EhsanMasoumipor <emasoumipor@gmail.com>
2024-03-17 17:17:17 +0330

    Handling Payment error in Parsi

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -10,11 +10,17 @@ connect();
 export async function POST(request) {
     try {
         const data = await request.json();
-        const { order, installment } = data;
-        const findOrder = await Order.findById(order);
+        const { order: orderId, installment } = data;
         const userId = await get_user_data_from_session(request);
-        const user = await User.findById(userId).select("_id username email notifications payments")
+        const user = await User.findById(userId).select("_id username email notifications payments");
 
+        const findOrder = await Order.findById(orderId).populate('user');
+        if (!findOrder || String(findOrder.user._id) !== userId) {
+            return NextResponse.json({
+                success: false,
+                message: "شناسه وارد شده با حساب شما مطابقت ندارد"
+            }, { status: 400 })
+        }
         const MESSAGE_CONTENT = {
             FULL_PAYMENT_MESSAGE: {
                 title: "هزینه سفارش به طور کلی پرداخت شد",
@@ -28,14 +34,19 @@ export async function POST(request) {
                 title: "قسط دوم پرداخت شد",
                 message: "شما قسط دوم از سفارش خود را پرداخت کردید"
             }
-        }
+        };
+
         let amount;
+        let paymentMessage;
         let newPayment;
 
         switch (installment) {
             case "fullPayment":
                 if (findOrder.paymentStatus.isFullPaid) {
-                    throw new Error("Order is already fully paid");
+                    return NextResponse.json({
+                        success: false,
+                        message: "تمامی هزینه سفارش مربوطه پرداخت شده است"
+                    }, { status: 400 })
                 }
                 amount = findOrder.totalPrice;
                 findOrder.installments.forEach(inst => {
@@ -43,53 +54,49 @@ export async function POST(request) {
                     inst.paidAt = Date.now();
                 });
                 findOrder.paymentStatus.isFullPaid = true;
-                const fullPaymentNotification = await sendNotification(
-                    MESSAGE_CONTENT.FULL_PAYMENT_MESSAGE.title,
-                    MESSAGE_CONTENT.FULL_PAYMENT_MESSAGE.message
-                )
-                user.notifications.push(fullPaymentNotification._id)
+                paymentMessage = MESSAGE_CONTENT.FULL_PAYMENT_MESSAGE;
                 break;
 
             case "firstInstallment":
             case "secondInstallment":
                 const index = installment === "firstInstallment" ? 0 : 1;
                 if (findOrder.installments[index].paid) {
-                    throw new Error(`Installment ${installment} is already paid`);
+                    return NextResponse.json({
+                        success: false,
+                        message: "هزینه قسط وارد کرده قبلا پرداخت شده بود"
+                    }, { status: 400 })
                 }
                 amount = findOrder.installments[index].amount;
                 findOrder.installments[index].paid = true;
                 findOrder.installments[index].paidAt = Date.now();
                 findOrder.paymentStatus.paidInsallments += 1;
                 findOrder.paymentStatus.totalPaidPrice += amount;
+                paymentMessage = installment === "firstInstallment" ? MESSAGE_CONTENT.FIRST_INSTALLMENT_PAID_MESSAGE : MESSAGE_CONTENT.SECOND_INSTALLMENT_PAID_MESSAGE;
                 break;
+
             default:
-                throw new Error("Invalid installment type");
+                return NextResponse.json({
+                    success: false,
+                    message: "لطفا مشخص کنید چه مقدار از هزینه سفارش را پرداخت میکنید"
+                }, { status: 400 });
         }
-        if (installment === "firstInstallment") {
-            const firstInstallmentPaidNotification = await sendNotification(
-                MESSAGE_CONTENT.FIRST_INSTALLMENT_PAID_MESSAGE.title,
-                MESSAGE_CONTENT.FIRST_INSTALLMENT_PAID_MESSAGE.message
-            )
-            user.notifications.push(firstInstallmentPaidNotification._id)
-        } else if (installment === "secondInstallment") {
-            const secondInstallmentPaidNotification = await sendNotification(
-                MESSAGE_CONTENT.SECOND_INSTALLMENT_PAID_MESSAGE.title,
-                MESSAGE_CONTENT.SECOND_INSTALLMENT_PAID_MESSAGE.message
-            )
-            user.notifications.push(secondInstallmentPaidNotification._id)
-        }
-        newPayment = await Payment.create({ order, installment, amount });
-        user.payments.push(newPayment._id)
+
+        const paymentNotification = await sendNotification(paymentMessage.title, paymentMessage.message);
+        user.notifications.push(paymentNotification._id);
+
+        newPayment = await Payment.create({ order: orderId, installment, amount });
+        user.payments.push(newPayment._id);
 
         await Promise.all([
             findOrder.save(),
             user.save()
         ]);
-        return NextResponse.json({ newPayment }, { status: 201 })
+
+        return NextResponse.json({ newPayment }, { status: 201 });
     } catch (error) {
         return NextResponse.json({
             success: false,
             message: error.message
-        }, { status: 500 })
+        }, { status: 500 });
     }
 }
