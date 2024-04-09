863be53d4ce184a8d213e0234c321188a951ea44
EhsanMasoumipor <emasoumipor@gmail.com>
2024-04-10 02:25:03 +0330

    Modify create order api route , optimize the route

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -11,11 +11,10 @@ connect();
 export async function POST(request) {
   try {
     const data = await request.json();
-    let { planName, totalPrice } = data;
+    const { planName, totalPrice } = data;
 
-
-    const user_id = await get_user_data_from_session(request);
-    const user = await User.findOne({ _id: user_id });
+    const userId = await get_user_data_from_session(request);
+    const user = await User.findOne({ _id: userId });
 
     const plan = await Plan.findOne({ name: planName });
 
@@ -26,43 +25,35 @@ export async function POST(request) {
       selectedFeatures = selectedFeatures.concat(data.selectedFeatures);
     }
 
+    const firstInstallmentAmount = parseFloat((totalPrice * 0.4).toFixed(2));
+    const secondInstallmentAmount = parseFloat((totalPrice * 0.6).toFixed(2));
     const newOrder = await Order.create({
       plan: planName,
       user,
       selectedFeatures,
-      totalFeature: selectedFeatures.length,
-      statusDates: {
-        pending: new Date(Date.now())
-      },
-      totalPrice
+      statusDates: { pending: new Date() },
+      totalPrice,
+      installments: [ 
+        { amount: firstInstallmentAmount },
+        { amount: secondInstallmentAmount }
+      ]
     });
-
-    if (!newOrder.installments.find(inst => inst.amount === newOrder.totalPrice * 0.4)) {
-      const firstInstallmentAmount = newOrder.totalPrice * 0.4;
-      newOrder.installments.push({ amount: firstInstallmentAmount });
-    }
-
-    if (!newOrder.installments.find(inst => inst.amount === newOrder.totalPrice * 0.6)) {
-      const secondInstallmentAmount = newOrder.totalPrice * 0.6;
-      newOrder.installments.push({ amount: secondInstallmentAmount });
-    }
 
     const newNotification = await sendNotification(
       "سفارش جدید با موفقیت ثبت شد",
-      "شما یک وبسایت جدید ثبت کردید, پس از برسی آن ما به شما وضعیت آنرا اطلاع خواهیم داد"
+      "شما یک وبسایت جدید ثبت کردید. پس از بررسی آن، به شما وضعیت آنرا اطلاع خواهیم داد."
     );
 
-    user.notifications.push(newNotification);
+    user.notifications.push(newNotification._id);
     user.orders.push(newOrder._id);
 
     await user.save();
-    await newOrder.save();
 
     return NextResponse.json({ newOrder }, { status: 201 });
   } catch (error) {
     return NextResponse.json({
       success: false,
       message: error.message
-    }, { status: 500 })
+    }, { status: 500 });
   }
 }
