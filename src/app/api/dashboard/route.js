8ead127d80e0c1666e17f9004552242b5930749f
EhsanMasoumipor <emasoumipor@gmail.com>
2024-04-12 16:48:56 +0330

    Modify user cache (cache in userId cache)

--- src/app/api/dashboard/route.js
+++ src/app/api/dashboard/route.js
@@ -10,12 +10,12 @@ const nodeCache = new NodeCache();
 export async function GET(request) {
     try {
         let user;
-        if(nodeCache.has("user")){
-            user = JSON.parse(nodeCache.get("user"))
+        const userId = await get_user_data_from_session(request);
+        if(nodeCache.has(userId)){
+            user = JSON.parse(nodeCache.get(userId))
         } else {
-            const userId = await get_user_data_from_session(request);
             user = await User.findOne({ _id: userId });
-            nodeCache.set("user", JSON.stringify(user), 3600)
+            nodeCache.set(userId, JSON.stringify(user), 3600)
         }
         return NextResponse.json({ user }, { status: 200 })
     } catch (error) {
@@ -40,7 +40,7 @@ export async function PUT(request) {
             new: true,
             runValidators: true,
         })
-
+        nodeCache.del(userId)
         return NextResponse.json({ newUserInfo }, { status: 200 });
     } catch (error) {
         return NextResponse.json({
