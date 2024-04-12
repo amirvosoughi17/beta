b92e45e2cd1c2de488f9056d81e96a0f86e9ac32
EhsanMasoumipor <emasoumipor@gmail.com>
2024-04-12 16:43:27 +0330

    Remove Next-auth, nodemailer, and sendMail util

--- src/app/api/jobs/paymentReminder/route.js
+++ src/app/api/jobs/paymentReminder/route.js
@@ -4,82 +4,9 @@ import { User } from "@/models/User";
 import { sendNotification } from "@/utils/sendNotification";
 import { NextResponse } from "next/server";
 import cron from 'node-cron';
-
 connect();
 // 0 */1 * * *
 export async function GET() {
-    try {
-        cron.schedule('* * * * *', async () => {
-            console.log('paymentReminder job is running ...');
 
-            const orders = await Order.find({
-                status: { $in: ["در انتظار پرداخت قسط اول", "در انتظار پرداخت قسط دوم"] }
-            });
-
-            for (const order of orders) {
-                const unpaidInstallments = order.installments.filter(inst => !inst.paid);
-
-                if (unpaidInstallments.length > 0) {
-                    const overdueDate = new Date();
-                    overdueDate.setDate(overdueDate.getDate() - 3);
-
-                    const oneDayLaterDate = new Date(overdueDate);
-                    oneDayLaterDate.setDate(oneDayLaterDate.getDate() + 1);
-
-                    const twoDaysAgoDate = new Date(overdueDate);
-                    twoDaysAgoDate.setDate(twoDaysAgoDate.getDate() + 2);
-
-                    if (new Date() >= overdueDate && new Date() < oneDayLaterDate) {
-                        await dayReminder(order, "روز آخر");
-
-                    } else if (new Date() >= oneDayLaterDate && new Date() < twoDaysAgoDate) {
-                        await dayReminder(order, "دو روز مانده");
-
-                    } else if (new Date() >= twoDaysAgoDate) {
-                        await dayReminder(order, "یک روز مانده");
-                    }
-                }
-            }
-        })
-        return NextResponse.json({ message: "paymentReminder is running..." }, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({
-            success: false,
-            message: error.message
-        }, { status: 500 })
-    }
 }
 
-async function dayReminder(order, day) {
-    const lastReminderSentAt = order.lastReminderSentAt || new Date(0);
-
-    if ((new Date() - lastReminderSentAt) < (24 * 60 * 60 * 1000)) {
-        return;
-    }
-
-    const user = await User.findById(order.user);
-    let notificationMessage = "";
-    switch (day) {
-        case 'روز آخر':
-            notificationMessage = "شما فراموش کرده‌اید مبلغ قسط خود را تا امروز پرداخت کنید";
-            break;
-        case 'دو روز مانده':
-            notificationMessage = 'یادآوری: دو روز پیش بود که باید مبلغ قسط خود را پرداخت کنید';
-            break;
-        case 'یک روز مانده':
-            notificationMessage = "یادآوری: فردا باید مبلغ قسط خود را پرداخت کنید.";
-            break;
-        default:
-            notificationMessage = "شما برای سفارش خود مبلغ قسط‌های خود را پرداخت نکرده‌اید. لطفاً به سرعت مبلغ معوقه را پرداخت کنید.";
-            break;
-    }
-    const reminderNotification = await sendNotification(
-        "یادآوری پرداخت",
-        notificationMessage
-    );
-    order.lastReminderSentAt = new Date();
-    await order.save();
-
-    user.notifications.push(reminderNotification._id);
-    await user.save();
-} 
